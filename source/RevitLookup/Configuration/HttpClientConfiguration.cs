0b5f63d46ed6ca3812eb49fcabc08872c831e100
Nice3point <nice3point@gmail.com>
2026-03-11 22:37:03 +0300

    [Configuration] Reorder HttpClientDefaults configuration.

--- source/RevitLookup/Configuration/HttpClientConfiguration.cs
+++ source/RevitLookup/Configuration/HttpClientConfiguration.cs
@@ -9,9 +9,9 @@ public static class HttpClientConfiguration
 {
     public static TBuilder ConfigureHttpClients<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
-        builder.Services.ConfigureHttpClientDefaults(clientBuilder => clientBuilder.RemoveAllLoggers());
         builder.Services.AddHttpClient("GitHubSource", client => client.BaseAddress = new Uri("https://api.github.com/repos/jeremytammik/RevitLookup/"));
         
+        builder.Services.ConfigureHttpClientDefaults(clientBuilder => clientBuilder.RemoveAllLoggers());
         builder.Services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
         
         return builder;
